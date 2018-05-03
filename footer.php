547e48803a86d17d15225def34f206970c964266
Christian Wach <needle@haystack.co.uk>
2018-05-03 13:24:05 +0100

    refresh translation file

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 		<?php endif; ?>
 
 		<p><?php echo sprintf(
-			__( 'Website content &copy; %1$s %2$s. All rights reserved.', 'commentpress-core' ),
+			__( 'Website content &copy; %1$s %2$s. All rights reserved.', 'commentpress-sof-de' ),
 			'<a href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>',
 			date('Y')
 		); ?></p>
