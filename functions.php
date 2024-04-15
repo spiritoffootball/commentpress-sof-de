84e9d94c8afef120b494fbc1795e3b58b40f9149
Christian Wach <needle@haystack.co.uk>
2024-04-15 16:08:21 +0100

    Bump version to 1.4.7a

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Set our version here.
-define( 'COMMENTPRESS_SOF_DE_VERSION', '1.4.6' );
+define( 'COMMENTPRESS_SOF_DE_VERSION', '1.4.7a' );
 
 // Damn this content width thing.
 if ( ! isset( $content_width ) ) {
